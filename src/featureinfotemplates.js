468092f23004a2dee6a024e486650f652b35dc3d
Grammostola <perssond9@gmail.com>
2023-01-09 15:18:35 +0100

    feat: add additional featureinfo template

--- src/featureinfotemplates.js
+++ src/featureinfotemplates.js
@@ -1,7 +1,9 @@
 import defaultTemplate from './templates/featureinfotemplate';
+import hyperlinkTemplate from './templates/hyperlinktemplate';
 
 const templates = {};
 templates.default = defaultTemplate;
+templates.hyperlink = hyperlinkTemplate; 
 
 // #region EK-specific for featureinfo with FTL 
 const textHtmlTemplate = function textHtmlTemplate(attributes) {
